8a67656f317a07258de3642350e17ef8e182ddc9
Rachma-Nurhaliza-Parindra <rachmanurhalizastudent@gmail.com>
2024-02-12 10:22:34 +0700

    update

---
+++ jscroot/controller/map/template.js
@@ -0,0 +1,16 @@
+export let xxx = "https://us-central1-spheric-entity-401507.cloudfunctions.net/function-8";
+export let aaa = "https://us-central1-spheric-entity-401507.cloudfunctions.net/func-api";
+export let bbb = "/gispostlinestring";
+export let ccc = "/gitpostpolygon";
+export let qqq = "https://asia-southeast2-gisuas-413104.cloudfunctions.net/geointersect";
+export let www = "https://asia-southeast2-gisuas-413104.cloudfunctions.net/geowithinX";
+export let eee = "https://asia-southeast2-gisuas-413104.cloudfunctions.net/near";
+export let rrr = "https://asia-southeast2-gisuas-413104.cloudfunctions.net/nearsphere";
+export let ttt = "https://asia-southeast2-gisuas-413104.cloudfunctions.net/box";
+export let yyy = "https://asia-southeast2-gisuas-413104.cloudfunctions.net/center";
+export let uuu = "/postcentersphere";
+
+export let clickpopup = `
+Coordinate  : #COORDINATE#<br>
+Type  : #TYPE#<br>
+`
