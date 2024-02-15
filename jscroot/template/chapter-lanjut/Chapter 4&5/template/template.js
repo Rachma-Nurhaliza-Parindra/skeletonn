8c57f8d169aea0cb353655ab3ef9c86398078860
Rachma-Nurhaliza-Parindra <rachmanurhalizastudent@gmail.com>
2024-02-15 17:51:47 +0700

    update

---
+++ jscroot/template/chapter-lanjut/Chapter 4&5/template/template.js
@@ -0,0 +1,13 @@
+export let URLGeoJson = "https://us-central1-spheric-entity-401507.cloudfunctions.net/function-8";
+export let URLPostPoint = "https://us-central1-spheric-entity-401507.cloudfunctions.net/func-api";
+export let tableTag="tr";
+export let tableRowClass="content is-small";
+export let tableTemplate=`
+<td>#NAME#</td>
+<td >#KORDINAT#</td>
+<td>#TYPE#</td>
+`
+export const clickpopup = `
+Long : #LONG#<br>
+Lat  : #LAT#<br>
+`
